7daa7d7f4da688869eb5b945364ebc5e1a42834b
Johan Magnusson <johan.magnusson@mentimeter.com>
2026-01-27 11:29:32 +0100

    Don't share history between sessions

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -4,7 +4,6 @@ HISTSIZE=10000000
 SAVEHIST=10000000
 
 # History options
-setopt SHARE_HISTORY          # Share history between sessions
 setopt INC_APPEND_HISTORY     # Add commands immediately
 setopt HIST_IGNORE_DUPS       # Don't store duplicates
 setopt HIST_IGNORE_SPACE      # Ignore commands starting with space
