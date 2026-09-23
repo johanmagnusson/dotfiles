e0c2a9270277b8b3ad697ee9a817dbd378874e1d
Johan Magnusson <johan.b.magnusson@gmail.com>
2026-09-23 12:57:32 +0200

    Update pnpm config with latest version of env

--- zsh/env/pnpm.zsh
+++ zsh/env/pnpm.zsh
@@ -1,6 +1,5 @@
-export PNPM_HOME="/Users/johan.magnusson/.pnpm"
-
+export PNPM_HOME='/Users/johan.magnusson/.pnpm'
 case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
+  *":$PNPM_HOME/bin:"*) ;;
+  *) export PATH="$PNPM_HOME/bin:$PATH" ;;
 esac
